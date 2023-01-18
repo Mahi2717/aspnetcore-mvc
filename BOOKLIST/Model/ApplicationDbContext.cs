ec92a99108645981a56e5f594dad463ba8921d1c
Mahi2717 <mahimaspatil37@gmail.com>
2023-01-18 14:34:51 +0530

    aspnetcore-mvc

---
+++ BOOKLIST/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BOOKLIST.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
